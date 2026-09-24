bf475b9e23f9a9610759e94b3aeb757daa1cb67a
AkashS-123 <128276097+AkashS-123@users.noreply.github.com>
2026-09-24 11:06:34 +0530

    third commit

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://cmchatapp-1.onrender.com';
+const API_URL = 'http://localhost:4000';
 
 async function request(path, options = {}) {
   const res = await fetch(`${API_URL}${path}`, {
